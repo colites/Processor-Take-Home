20b5367f435c1e68093912c95ede82391dae6348
colites <ezequielgomez2022@u.northwestern.edu>
2023-09-20 15:32:54 -0500

    updated main.go
    added checks for having the /points for the GET request

--- receipt-processor/cmd/server/main.go
+++ receipt-processor/cmd/server/main.go
@@ -27,16 +27,24 @@ func main() {
 	// Handles the "/receipts/" route for getting points associated with a receipt ID.
 	// Accepts only GET requests.
 	http.HandleFunc("/receipts/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			// Extract the ID from the URL
-			id := strings.TrimPrefix(r.URL.Path, "/receipts/")
-			if id == "" {
-				http.Error(w, "Missing ID", http.StatusBadRequest)
-				return
-			}
+		if r.Method != "GET" {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// Extract the ID and optional "points" subpath from the URL
+		pathSegments := strings.Split(strings.TrimPrefix(r.URL.Path, "/receipts/"), "/")
+		id := pathSegments[0]
+		if id == "" {
+			http.Error(w, "Missing ID", http.StatusBadRequest)
+			return
+		}
+
+		// If there's a "points" subpath, call GetPoints; otherwise do not allow
+		if len(pathSegments) > 1 && pathSegments[1] == "points" {
 			handler.GetPoints(w, r, id)
 		} else {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, "Method or Path not allowed", http.StatusMethodNotAllowed)
 		}
 	})
 
